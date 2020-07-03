9bc57b7dc57a72ed1772bba8241dc0bfad4b69dd
SaiAshish9 <saiashish3760@gmail.com>
2020-07-04 02:14:54 +0530

    fixed bugs

--- src/components/desktop/Section1/index.js
+++ src/components/desktop/Section1/index.js
@@ -6,11 +6,11 @@ import PinkGrid from "../../../assets/svgs/pinkGrid";
 // import BusinessCard from "./businessCard";
 // import NotificationCard from "./notifications";
 // import ProfileCard from "./profile";
-import VerifyStatus from "../../../test/verifyStatus.png";
-import Card from "../../../test/card.png";
-import BusinessDev from "../../../test/businessDev.png";
-import Notifications from "../../../test/notifications.png";
-import ProfileScore from "../../../test/profileScore.png";
+import VerifyStatus from "../../../assets/test1/verifyStatus.png";
+import Card from "../../../assets/test1/card.png";
+import BusinessDev from "../../../assets/test1/businessDev.png";
+import Notifications from "../../../assets/test1/notifications.png";
+import ProfileScore from "../../../assets/test1/profileScore.png";
 
 const Section1 = () => (
   <Row
