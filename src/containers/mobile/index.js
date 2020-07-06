50d2454017e40a5ff5e55fbebef5da59933e3712
SaiAshish9 <saiashish3760@gmail.com>
2020-07-06 16:59:31 +0530

    fixed bugs

--- src/containers/mobile/index.js
+++ src/containers/mobile/index.js
@@ -1,28 +1,17 @@
-import React,{Suspense, lazy} from 'react'
-const GetStarted =lazy(() =>import('../../components/mobile/GetStarted'))
-const Navbar =lazy(() =>import("../../components/mobile/Navbar"))
-const Tasks=lazy(()=>import("../../components/mobile/Tasks"))
-const Section3=lazy(()=>import("../../components/mobile/Section3"))
-const Experience=lazy(()=>import("../../components/mobile/Experience"))
-const Badges=lazy(()=>import("../../components/mobile/Badges"))
-const JoinUs=lazy(()=>import("../../components/mobile/JoinUs"))
-const Footer=lazy(()=>import("../../components/mobile/Footer"))
+import React, { Suspense, lazy } from "react";
+import { Switch, Route } from "react-router-dom";
+const Homepage = lazy(() => import("./Homepage"));
+const CompanyPage = lazy(() => import("../desktop/Company_page"));
 
-const Mobile= () => {
-    return (
-      <Suspense fallback={<div></div>}>
-        <div style={{overflowX: 'hidden'}}>
-            <Navbar />
-            <GetStarted/>
-            <Tasks/>
-            <Section3/>
-            <Experience />
-            <Badges />
-            <JoinUs/>
-            <Footer />
-        </div>
-      </Suspense>
-    );
-}
+const Mobile = () => {
+  return (
+    <Suspense fallback={<div></div>}>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/company" component={CompanyPage} />
+      </Switch>
+    </Suspense>
+  );
+};
 
-export default Mobile
+export default Mobile;
